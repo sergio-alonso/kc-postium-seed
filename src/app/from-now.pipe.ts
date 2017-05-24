98a302def726d5f1ca04a9fdcf16c304dd861045
Sergio Alonso <sergio@sergioalonso.es>
2017-05-25 01:43:22 +0200

    Create FromNowPipe pipe

--- src/app/from-now.pipe.ts
+++ src/app/from-now.pipe.ts
@@ -1,13 +1,11 @@
+import { PipeTransform, Pipe } from '@angular/core';
 import * as moment from 'moment';
 import 'moment/locale/es';
 
-/*---------------------------------------------------------------------------------------------------------|
- | ~~~ Blue Path ~~~                                                                                       |
- |---------------------------------------------------------------------------------------------------------|
- | Crea el pipe FromNowPipe. Su cometido es, partiendo de una fecha dada, retornar una cadena de texto     |
- | que exprese el tiempo que ha pasado desde dicha fecha hasta ahora. Por ejemplo: hace 2 horas. Para esta |
- | tarea nos apoyamos en la librería Moment.js; ya tienes hecho el import correspondiente, solo tienes que |
- | usarla donde proceda. Haciendo 'moment(fecha).fromNow()' obtenemos justo lo que necesitamos.            |
- |---------------------------------------------------------------------------------------------------------*/
-
-export class FromNowPipe { }
+@Pipe({name: 'fromNow'})
+export class FromNowPipe implements PipeTransform {
+  transform(value, args){
+    let mydate = moment(new Date(value));
+    return mydate.fromNow();
+  }
+}
