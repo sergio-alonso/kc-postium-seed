682b936ab5931c2cf713b1cdb604e0681587be2c
Sergio Alonso <sergio@sergioalonso.es>
2017-05-25 12:12:45 +0200

    Expose output attribute

--- src/app/post-preview/post-preview.component.ts
+++ src/app/post-preview/post-preview.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 import { Post } from './../post';
 
@@ -19,13 +19,11 @@ export class PostPreviewComponent {
    | clic se realiza en el template de este componente, necesitas, además, un manejador para el mismo.                |
    |------------------------------------------------------------------------------------------------------------------*/
 
-  /*------------------------------------------------------------------------------------------------------------------|
-   | ~~~ Green Path ~~~                                                                                               |
-   |------------------------------------------------------------------------------------------------------------------|
-   | Expón un atributo de salida con el decorador correspondiente. El tipo de dicho atributo debe permitir la emisión |
-   | de eventos; la idea es enviar al componente padre el post sobre el cuál se ha hecho clic. Y puesto que dicho     |
-   | clic se realiza en el template de este componente, necesitas, además, un manejador para el mismo.                |
-   |------------------------------------------------------------------------------------------------------------------*/
+    @Output() onClick = new EventEmitter<Post>();
+
+    onSelect(post: Post) : void {
+	this.onClick.emit(post);
+    }
 
   plainTextToHtml(text: string): string {
     return text ? `<p>${text.replace(/\n/gi, "</p><p>")}</p>` : '';
