25534d9e11798727350f106192dda6fe8c4c57b1
Sergio Alonso <sergio@sergioalonso.es>
2017-05-27 22:04:41 +0200

    Resolve route to get posts by category

--- src/app/posts-resolve.service.ts
+++ src/app/posts-resolve.service.ts
@@ -12,17 +12,13 @@ export class PostsResolveService implements Resolve<Post[]> {
 
   resolve(route: ActivatedRouteSnapshot): Observable<Post[]> {
 
-      if(route.url[1] !== undefined && route.url[1].path === 'users') {
+      if(route.params["userId"]) {
 	  return this._postService.getUserPosts(route.params["userId"]);
       }
 
-    /*-----------------------------------------------------------------------------------------|
-     | ~~~ Yellow Path ~~~                                                                     |
-     |-----------------------------------------------------------------------------------------|
-     | Modifica este Resolve para que, en caso de tener que obtener los posts correspondientes |
-     | a una categoría, llame a la función 'getCategoryPosts()' del servicio PostService.      |
-     | Recuerda mirar en los parámetros de la ruta, a ver qué encuentras.                      |
-     |-----------------------------------------------------------------------------------------*/
+      if(route.params["categoryId"])
+	  return this._postService.getCategoryPosts(route.params["categoryId"]);
+      }
 
     return this._postService.getPosts();
   }
